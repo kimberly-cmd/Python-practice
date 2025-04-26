55faa842ee3f747cd75ffc558873ed7c5e0e382b
kimberly-cmd <kimberly.shammah@strathmore.edu>
2025-04-26 19:32:25 +0300

    Add files via upload

---
+++ EvenOrOdd.py
@@ -0,0 +1,10 @@
+x = input("Enter a number: ")
+#Converting the string x into an integer
+y = int(x)
+
+#To test for if the number is odd or even
+if y%2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+#The number is even if the remainder gotten after dividing it by 2 is 0 else it is odd
