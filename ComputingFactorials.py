a5e1e79d8fc00c636f652d124f8078c311600322
kimberly-cmd <kimberly.shammah@strathmore.edu>
2025-04-26 21:27:40 +0300

    Update ComputingFactorials.py

--- ComputingFactorials.py
+++ ComputingFactorials.py
@@ -1,7 +1,5 @@
+import math #To compute factorials directly
 x = input("Enter a number to find it's factorial: ")
-y = int(x)
-i = 1
-while i < y:
-    z = y * i
-    i += 1
-print(z)
+y = int(x) #To convert x from string to an integer
+a = math.factorial(y)
+print(a)
